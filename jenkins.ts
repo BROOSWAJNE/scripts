bc7c4003f24ef68f77f4c1c81d8ba3d1c418270c
Willem Garnier <willem.garnier@checkit.net>
2021-03-31 15:02:05 +0100

    save jenkins directory->job mapping

--- jenkins.ts
+++ jenkins.ts
@@ -1,4 +1,4 @@
-#! /usr/bin/env deno run --allow-env --allow-net
+#! /usr/bin/env deno run --allow-env --allow-net --allow-read --allow-write
 
 import {
 	UNNAMED_ARGUMENTS,
@@ -6,12 +6,17 @@ import {
 } from './common/args.ts';
 import {
 	bold,
+	cyan,
+	dim,
 	green,
 	magenta,
 	red,
-	dim,
 } from './common/colors.ts';
 import { required } from './common/env.ts';
+
+// Constants
+
+type Mapping = Record<string, string | void>;
 
 const JENKINS_API_USER = await required('JENKINS_API_USER', 'Your Jenkins username.');
 const JENKINS_API_TOKEN = await required('JENKINS_API_TOKEN', 'Your Jenkins API token - generate it on the settings page.');
@@ -20,11 +25,74 @@ const JENKINS_URL_BASE = await required('JENKINS_URL_BASE', 'Your Jenkins instan
 const AUTH_FIELDS = `${JENKINS_API_USER}:${JENKINS_API_TOKEN}`;
 const AUTH_HEADER = `Basic ${btoa(AUTH_FIELDS)}`;
 
-const abort = (reason: string) => (console.log(red(reason)), Deno.exit(1));
+const USAGE = [
+	`${bold('Usage')}: jenkins.ts [job] <...options>`,
+	'',
+	'Options:',
+	`  ${cyan('-s --save')} Associates the current working directory with the given job, so that a job name isn\'t required next time.`,
+].join('\n');
 
+// Utility Functions
+
+let mappingPath: string | null = null;
+
+function abort(reason: string) {
+	console.log(red(reason));
+	Deno.exit(1);
+}
+async function getMappingPath( ) {
+	if (!mappingPath) mappingPath = await required(
+		'JENKINS_JOBS_MAPPING_PATH', 
+		'A path to a file which this script can edit to save a Jenkins job <-> directory mapping.',
+	);
+	return mappingPath;
+}
+async function readMapping( ): Promise<Mapping> {
+	const file = await getMappingPath( );
+
+	console.log(dim(`Reading existing mapping at: ${bold(file)}`));
+	const data = await Deno.readFile(file);
+	const json = new TextDecoder( ).decode(data);
+	const mapping = JSON.parse(json);
+
+	// validate
+	if (typeof mapping !== 'object') abort('Invalid mapping file contents');
+	if (mapping == null) abort('Invalid mapping file contents');
+	if (Array.isArray(mapping)) abort('Invalid mapping file contents');
+
+	return mapping;
+}
+async function saveMapping(mapping: Mapping) {
+	const file = await getMappingPath( );
+
+	console.log(dim(`Saving updated mapping to: ${bold(file)}`));
+	const updated = new TextEncoder( ).encode(JSON.stringify(mapping, null, 2));
+	await Deno.writeFile(file, updated);
+}
+
+// Script
+
+const cwd = Deno.cwd( );
 const args = parse(Deno.args);
-const [ job ] = args[UNNAMED_ARGUMENTS];
+
+const shouldHelp = args['help'] || args['h'];
+const shouldSave = args['save'] || args['s'];
+
+if (shouldHelp) (console.log(USAGE), Deno.exit(0));
+
+const job = args[UNNAMED_ARGUMENTS][0] ?? await (async function getJobFromMapping( ) {
+	console.log(dim('No job specifed - attempting to read from mapping file.'));
+	const mapping = await readMapping( );
+	return mapping[cwd];
+}( ));
 if (!job) abort('No job specified');
+
+if (shouldSave) {
+	const mapping = await readMapping( );
+	console.log(`Saving mapping: ${cyan(cwd)} -> ${magenta(job)}`);
+	mapping[cwd] = job;
+	await saveMapping(mapping);
+}
 
 console.log(`Starting job: ${bold(magenta(job))}`);
 
